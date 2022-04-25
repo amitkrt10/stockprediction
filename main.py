ef0885e498eff0f5414b534c92e22bba56192540
amitkrt10 <46473863+amitkrt10@users.noreply.github.com>
2022-04-25 11:24:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Stock Forecast App')
 
-stocks = ('GOOG', 'AAPL', 'MSFT', 'GME', 'TCS')
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME', 'TCS', 'INFY')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
 n_years = st.slider('Years of prediction:', 1, 4)
